0766d219ae5c50d0930cdcb04aca09903861a7c0
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-08-18 13:29:47 +0300

    initialized Eclinic project

---
+++ src/test/java/com/example/Eclinic/EclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
